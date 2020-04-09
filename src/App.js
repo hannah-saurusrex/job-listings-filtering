6dee2ca3e3e05f7f80a36efe564c43633a3baeab
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-04-09 12:30:42 -0600

    added a job board component and pulled in the component to the app file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import data from './assets/data.json';
+import JobBoardComponent from './components/JobBoardComponent';
+
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    setJobs(data);
+  }, []);
+
+  console.log(jobs);
+
   return (
     <div className="App">
       <h1>Hello</h1>
+      {
+        jobs.length === 0 ? (
+          <p>Jobs are fetching...</p>
+        ) : (
+          jobs.map(job => (
+            <JobBoardComponent job={job} key={job.id} />
+          ))
+        )
+      }
     </div>
   );
 }
 
 export default App;
+
+// ToDos
+// 1. study the design & data ✅
+// 2. create job board component (we have an array of objects) ✅
+// 3. pull in the data fron the JSON file ✅
+// 4. pass down the data from the job board component ✅
+// 5. style it
+// 6. filter component
+// 7. filter data
---
+++ src/components/JobBoardComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const JobBoardComponent = () => (
+    <div>Hi, i'm a job board!</div>
+)
+
+export default JobBoardComponent;
